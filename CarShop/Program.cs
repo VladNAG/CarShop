4bb943a1cbed28b988c1c42824b41e01d3ff8550
Vladyslav Nahornyi <vladnagornyj2@gmail.com>
2023-10-31 16:13:48 +0000

    Revert "add Identity"
    This reverts commit dae274c6d4c5ebd7d4d1ff7d459631dd9d0745ef.

--- CarShop/Program.cs
+++ CarShop/Program.cs
@@ -1,63 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
-using Microsoft.AspNetCore.Authentication;
-using System.IdentityModel.Tokens.Jwt;
+var app = builder.Build();
 
-
-public class Program
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
-
-        builder.Services.AddAuthentication(options =>
-        {
-            options.DefaultScheme = "Cookies";
-            options.DefaultChallengeScheme = "oidc";
-        })
-            .AddCookie("Cookies")
-            .AddOpenIdConnect("oidc", options =>
-            {
-                options.Authority = "https://localhost:5001";
-
-                options.ClientId = "web";
-                options.ClientSecret = "secret";
-                options.ResponseType = "code";
-                options.Scope.Add("verification");
-                options.ClaimActions.MapJsonKey("email_verified", "email_verified");
-
-                options.Scope.Clear();
-                options.Scope.Add("openid");
-                options.Scope.Add("profile");
-                options.GetClaimsFromUserInfoEndpoint = true;
-
-                options.SaveTokens = true;
-            });
-        
-
-        // Add services to the container.
-        builder.Services.AddRazorPages();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapRazorPages().RequireAuthorization();
-        app.Run();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
